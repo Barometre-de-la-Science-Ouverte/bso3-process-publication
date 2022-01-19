6e6eb1409ae5e6dff023dd246a67bea9b7b64b44
ANBR <abres@octo.com>
2022-01-19 11:53:15 +0100

    Refacto

--- run_grobid.py
+++ run_grobid.py
@@ -3,7 +3,7 @@ import sys
 from glob import glob
 from config.path_config import CONFIG_PATH_GROBID, DATA_PATH
 
-def run_grobid(config_path, data_path):
+def run_grobid(config_path, data_path, GrobidClient):
     client = GrobidClient(config_path=config_path)
     client.process("processFulltextDocument", data_path, output=data_path, consolidate_citations=True, tei_coordinates=True, force=True, verbose=True, n=4)
 
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     sys.path.insert(1, os.path.join(sys.path[0], '..'))
     from grobid_client.grobid_client import GrobidClient
 
-    run_grobid(CONFIG_PATH_GROBID, DATA_PATH)
+    run_grobid(CONFIG_PATH_GROBID, DATA_PATH, GrobidClient)
 
--- run_softcite.py
+++ run_softcite.py
@@ -1,20 +1,16 @@
 import os
 import sys
-import requests
-from glob import glob
 from config.path_config import DATA_PATH, CONFIG_PATH_SOFTCITE
 
 
-def run_softcite(config_path, data_path):
-    # Ne s'execute pas sur des pdfs compressés (.gz) -> config_harvester compression: false
-    client = smc.software_mention_client(config_path)
+def run_softcite(config_path, data_path, smc):
+    client = smc(config_path)
     client.annotate_directory(data_path)
     client.diagnostic(full_diagnostic=False)
 
 
 if __name__ == "__main__":
-    sys.path.insert(1, os.path.join(sys.path[0], '..'))
-    from software_mentions_client import software_mention_client as smc
+    from software_mentions_client.client import software_mentions_client as smc
 
-    run_softcite(CONFIG_PATH_SOFTCITE, DATA_PATH)
+    run_softcite(CONFIG_PATH_SOFTCITE, DATA_PATH, smc)
 
